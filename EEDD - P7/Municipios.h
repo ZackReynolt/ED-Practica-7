e583308b117519d6d1f1192e8b52a34be965f1a3
Alfanjor <xalfx.gm@gmail.com>
2015-01-12 17:56:36 +0100

    Clase Municipios creada

---
+++ EEDD - P7/Municipios.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   Municipio.h
+ * Author: alf
+ *
+ * Created on 12 de enero de 2015, 17:47
+ */
+
+#ifndef MUNICIPIOS_H
+#define	MUNICIPIOS_H
+
+class Municipios {    
+public:
+    string  _nombre;
+    float   _lat;
+    float   _long;
+    float   _altitud;
+    int     _habitantes;
+    
+  
+    int habitantes() {
+        return _habitantes;
+    }
+};
+
+#endif	/* MUNICIPIO_H */
+
